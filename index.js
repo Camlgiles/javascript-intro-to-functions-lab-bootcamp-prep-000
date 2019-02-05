e2c59364f4118415e01ba1de371d456e8eef3179
remote-certificate-1570 <cgiles@gilescpa.com>
2019-02-05 08:29:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toLowerCase());
+  console.log(string.toUpperCase());
 }
 
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+
+
+var uppercase = ''
